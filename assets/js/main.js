af437a0905703c64224ade81d1dd5cb5f475b33d
jhubar <hubarjulien@gmail.com>
2023-08-25 16:02:31 +0200

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -298,6 +298,24 @@
   });
 
   /**
+  * Roem slider
+  */
+  new Swiper('.events-slider', {
+    speed: 1000,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
    * Testimonials slider
    */
   new Swiper('.testimonials-slider', {
@@ -323,6 +341,27 @@
         slidesPerView: 3,
         spaceBetween: 20
       }
+    }
+  });
+  /**
+ * Init swiper slider with 1 slide at once in desktop view
+ */
+  new Swiper('.slides-1', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     }
   });
 
